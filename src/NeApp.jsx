135c75e1c03398009cf81875b4c2e8c38b1e43fa
ilya <zapaska96@gmail.com>
2022-08-30 17:29:00 +0500

    hero без градиента

--- src/NeApp.jsx
+++ src/NeApp.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { GetStarted } from '../components';
+import styles from '../style';
 import { discount, robot } from '../assets';
-// import styles from '../style';
+import GetStarted from './GetStarted';
 
 const Hero = () => {
   return (
-    <section id="Home" className={`flex md:flex-row flex-col sm:py-16 py-6`}>
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+    >
       <div
-        className={`flex-1 flex justify-center items-start flex-col xl:px-0 sm:px-16 px-6`}
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-          <p
-            className={`font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] ml-2`}
-          >
+          <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">20%</span> Discount For{' '}
             <span className="text-white">1 Month</span> Account
           </p>
@@ -29,10 +30,10 @@ const Hero = () => {
           </div>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] leading-[75px] ss:leading-[100px] w-full text-white">
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
           Payment Method.
         </h1>
-        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Our team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.
@@ -40,21 +41,22 @@ const Hero = () => {
       </div>
 
       <div
-        className={`flex-1 flex justify-center items-center md:my-0 my-10 relative`}
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
       >
         <img
           src={robot}
           alt="billing"
-          className="w-full h-full relative z-10"
+          className="w-[100%] h-[100%] relative z-[5]"
         />
 
-        {/* -- Градиент под картинкой робота */}
-        {/* <div className="absolute z-0 w-[40%] h-[35%] top-0 pink__gradient" />
-        <div className="absolute z-0 w-[40%] h-[35%] top-0 white__gradient" />
-        <div className="absolute z-0 w-[40%] h-[35%] top-0 blue__gradient" /> */}
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
       </div>
 
-      <div className={`ss:hidden flex justify-center items-center`}>
+      <div className={`ss:hidden ${styles.flexCenter}`}>
         <GetStarted />
       </div>
     </section>
