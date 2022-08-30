837ffc4476d0bd3da462b5313cf0e068103dc6ba
ilya <zapaska96@gmail.com>
2022-08-30 17:47:06 +0500

    stats компонент, начальная страница готова

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { stats } from '../constants';
 
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section className="flex justify-center items-center flex-row flex-wrap sm:mb-20 mb-6">
+      {stats.map((item) => (
+        <div
+          key={item.id}
+          className="flex-1 flex justify-start items-center mt-4"
+        >
+          <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+            {item.value}
+          </h4>
+          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3">
+            {item.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Stats;
