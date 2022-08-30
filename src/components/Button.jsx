3d8e7a92934eb4b4d94d691bfa6f4ffda9673e54
ilya <zapaska96@gmail.com>
2022-08-30 19:01:26 +0500

    button компонент

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles, text }) => {
+  return (
+    <button
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-meduim text-lg text-primary outline-none ${styles} rounded-[10px]`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
