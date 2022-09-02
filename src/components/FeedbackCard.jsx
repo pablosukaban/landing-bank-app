3d7d692e901c485bc8f57f30ac43bb46ec74f6e4
ilya <zapaska96@gmail.com>
2022-09-02 15:10:09 +0500

    Testimonials компонент с переиспользуемой карточкой клиента

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { quotes } from '../assets';
 
-const FeedbackCard = () => {
-  return <div>FeedbackCard</div>;
+const FeedbackCard = ({ content, name, title, img }) => {
+  return (
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img
+        src={quotes}
+        alt="quotes"
+        className="w-[42px] h-[27px] object-contain m-0"
+      />
+      <p className="font-pippins font-normal text-lg leading-[32px] text-white m-0 my-10 tracking-[2%]">
+        {content}
+      </p>
+
+      <div className="flex items-center gap-4">
+        <img src={img} alt="avatar" className="w-12 h-12 rounded-full" />
+        <div className="flex flex-col">
+          <h4 className="text-white font-poppins font-normal text-xl leading-8">
+            {name}
+          </h4>
+          <p className="text-dimWhite font-poppins font-normal text-base leading-6">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackCard;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { feedback } from '../constants';
+import FeedbackCard from './FeedbackCard';
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section
+      id="clients"
+      className="sm:py-16 py-6 flex justify-center items-center flex-col relative"
+    >
+      {/* градиент */}
+      <div />
+
+      <div className="w-full flex items-center md:flex-row flex-col z-10 relative sm:mb-16 mb-6">
+        <h2 className="font-poppins font-semibold xs:text-5xl text-[40px] text-white xs:leading-[77px] leading[67px] w-full">
+          What people are <br className="sm:block hidden" /> saying about us
+        </h2>
+        <div className="w-full">
+          <p className="font-poppins font-normal text-dimWhite text-lg leading-[30px] max-w-[450px]">
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+        {feedback.map((item) => (
+          <FeedbackCard key={item.id} {...item} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
