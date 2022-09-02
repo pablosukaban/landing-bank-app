c51f7f479c2013ffa5bb8e147f791ec3d450cdab
ilya <zapaska96@gmail.com>
2022-09-02 14:05:47 +0500

    Card Deal компонент

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { card } from '../assets';
+import Button from './Button';
 
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className="flex md:flex-row flex-col sm:py-16 py-6">
+      <div className="flex-1 flex justify-center items-start flex-col">
+        <h2 className="font-poppins font-semibold xs:text-5xl text-[40px] text-white xs:leading-[77px] leading-[67px] w-full">
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className="mt-6 font-poppins font-normal text-dimWhite text-lg leading-[30px] max-w-[470px]">
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        </p>
+        <Button text="Get started" styles="mt-4" />
+      </div>
+
+      <div className="flex-1 flex justify-center items-center md:ml-10 ml-0 md:mt-0 mt-10 relative">
+        <img src={card} alt="card" className="w-full h-full" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
