026f96c54682f3595f450546e818883622f4d7c0
ilya <zapaska96@gmail.com>
2022-09-02 19:27:17 +0500

    client компонент

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { clients } from '../constants';
 
 const Client = () => {
-  return <div>Client</div>;
+  return (
+    <section className="flex justify-center items-center my-4">
+      <div className="flex justify-center items-center flex-wrap w-full">
+        {clients.map((item) => (
+          <div
+            key={item.id}
+            className="flex-1 flex justify-center items-center sm:min-w-[192px] min-w-[100px]"
+          >
+            <img
+              src={item.logo}
+              alt="client logo"
+              className="sm:w-[192px] w-[100px] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Client;
